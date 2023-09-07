3a90347a786fdd31cecfcc36ee60cfb5bdbe7edf
ErickNaves <ericknevesea@gmail.com>
2023-09-07 10:33:53 -0300

    change path

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = DB::select('SELECT nome FROM series;');
+
+        return view('series.index')->with('series', $series);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+
+        DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]);
+        return redirect("/series");
+    }
+}
+
