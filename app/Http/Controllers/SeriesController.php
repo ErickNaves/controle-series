c5c4b51aff7fb1980b0654d24bab2a9bb0ab139e
ErickNaves <ericknevesea@gmail.com>
2023-09-13 11:40:02 -0300

    create Series request

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeriesFormRequest;
 use App\Models\Serie;
 use App\Models\Series;
 use Illuminate\Http\Request;
@@ -23,17 +24,13 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
         // $nomeSerie = $request->nome;
         // $series = new Serie();
         // $series->nome = $nomeSerie;
         // $series->save();
         // A linha 31 executa basicamente o mesmo código feito da linha 26 à 29. (Existem algumas diferenças).
-
-        $request->validate([
-            'nome' => ['required', 'min:3']
-        ]);
 
         $series = Serie::create($request->all());
         
@@ -54,7 +51,7 @@ class SeriesController extends Controller
         return view('series.edit')->with('series',$series);
     }
 
-    public function update (Serie $series, Request $request)
+    public function update (Serie $series, SeriesFormRequest $request)
     {
         $series->nome = $request->nome;
         $series->save();
