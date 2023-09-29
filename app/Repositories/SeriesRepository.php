7be50384806609f8cc07c2641250d051e66d8916
ErickNaves <ericknevesea@gmail.com>
2023-09-29 08:56:41 -0300

    create SeriesProviders

--- app/Repositories/SeriesRepository.php
+++ app/Repositories/SeriesRepository.php
@@ -3,35 +3,9 @@
 namespace App\Repositories;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Episode;
-use App\Models\Season;
 use App\Models\Series;
-use Illuminate\Support\Facades\DB;
 
-class SeriesRepository
+interface SeriesRepository
 {
-    public function add(SeriesFormRequest $request): Series
-    {
-        return DB::transaction(function() use ($request){
-            $series = Series::create($request->all());
-            $seasons = [];
-            for ($i = 1; $i <= $request->seasonsQty; $i++) {
-                $seasons = [
-                    'series_id' => $series->id,
-                    'number' => $i,
-                ];
-                $season = Season::create($seasons);          
-                $episodes = [];
-                for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                    $episodes = [
-                        'season_id' => $season->id,
-                        'number' => $j
-                    ];
-                    Episode::create($episodes);
-                }
-            }
-
-            return $series;
-        });
-    }
+    public function add(SeriesFormRequest $request): Series;
 }
