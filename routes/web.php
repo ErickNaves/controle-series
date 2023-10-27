343fef175061ac09dbb1a881fde7429c803a6ac8
ErickNaves <ericknevesea@gmail.com>
2023-10-27 10:29:32 -0300

    add breeze on project

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
-use App\Http\Controllers\EpisodesController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\SeasonsController;
-use App\Http\Controllers\SeriesController;
-use App\Http\Controllers\UsersController;
-use App\Http\Middleware\Autenticador;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,30 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/series', SeriesController::class)->except(['show']);
-
-Route::middleware('autenticador')->group(function () {
-    Route::get('/', function () {
-        return redirect('/series');
-    });
-
-    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
-    Route::get('seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-    Route::post('seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store'])->name('signin');
-Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/register', [UsersController::class, 'create'])->name('users.create');
-Route::post('/register', [UsersController::class, 'store'])->name('users.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-
-// O código na linha 21 está fazendo o direcionamento das requisições para os lugares corretos como segue o exemplo nas linhas 24 à 27.
-// Route::controller(SeriesController::class)->group(function (){
-//     Route::get('/series', 'index')->name('series.index');
-//     Route::get('/series/create', 'create')->name('series.create');
-//     Route::post('/series/store', 'store')->name('series.store');
-// });
+require __DIR__.'/auth.php';
